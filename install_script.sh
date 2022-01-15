6141c02fc1e8b9eff846058797f5e4c93200720b
etzisim <admin@etzi.at>
2022-01-15 19:05:14 +0100

    Create install_script.sh

---
+++ install_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/etzisim/AutoUpgrade/main/autoupgrade.service --output /usr/lib/systemd/system/autoupgrade.service
+curl https://raw.githubusercontent.com/etzisim/AutoUpgrade/main/autoupgrade.timer --output /usr/lib/systemd/system/autoupgrade.timer
+curl https://raw.githubusercontent.com/etzisim/AutoUpgrade/main/upgrade.sh --output /opt/upgrade.sh
+
+systemctl daemon-reload
+systemctl enable --now autoupgrade.timer
+systemctl start autoupgrade.service
+
+exit 0

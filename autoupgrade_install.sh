d2e88a7ed349397da263fd142db58faebc70e8b2
etzisim <admin@etzi.at>
2022-01-15 19:12:00 +0100

    Update autoupgrade_install.sh

--- autoupgrade_install.sh
+++ autoupgrade_install.sh
@@ -4,6 +4,9 @@ curl https://raw.githubusercontent.com/etzisim/AutoUpgrade/main/autoupgrade.serv
 curl https://raw.githubusercontent.com/etzisim/AutoUpgrade/main/autoupgrade.timer --output /usr/lib/systemd/system/autoupgrade.timer
 curl https://raw.githubusercontent.com/etzisim/AutoUpgrade/main/upgrade.sh --output /opt/upgrade.sh
 
+pacman -Sy
+pacman -S pacman-contrib
+
 systemctl daemon-reload
 systemctl enable --now autoupgrade.timer
 systemctl start autoupgrade.service
